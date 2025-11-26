92120130c2cc7f17f2fd22ab87fe25777911e7b2
Deyutzu <popenciucandrei@gmail.com>
2025-11-26 21:53:58 +0200

    am reusit implementarea listei de rezervari si am adaugat si o functie de anulare

--- cazare/urls.py
+++ cazare/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # Cand adresa e goala (''), cheama functia homepage
     path('', views.homepage, name='acasa'),
     path('rezervare-noua/', views.creare_rezervare, name='creare_rezervare'),
+    path('rezervari/', views.lista_rezervari, name='lista_rezervari'),
+    path('rezervare/anulare/<int:rezervare_id>/', views.anuleaza_rezervare, name='anuleaza_rezervare'),
 ]
--- cazare/views.py
+++ cazare/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect  # <-- Am adaugat redirect
-from .models import TipCamera
+from django.shortcuts import render, redirect,get_object_or_404 # <-- Am adaugat redirect
+from .models import TipCamera,Rezervare,Camera
 from .forms import RezervareForm # <-- Am importat formularul creat la pasul 1
 
 def homepage(request):
@@ -20,3 +20,23 @@ def creare_rezervare(request):
         form = RezervareForm()
 
     return render(request, 'cazare/creare_rezervare.html', {'form': form})
+
+def lista_rezervari(request):
+    # Luam toate rezervarile, ordonate dupa data (cele mai recente primele)
+    rezervari = Rezervare.objects.all().order_by('-data_check_in')
+    
+    context = {
+        'rezervari': rezervari
+    }
+    return render(request, 'cazare/lista_rezervari.html', context)
+
+def anuleaza_rezervare(request, rezervare_id):
+    # 1. Cautam rezervarea dupa ID
+    rezervare = get_object_or_404(Rezervare, id=rezervare_id)
+    
+    # 2. Modificam statusul
+    rezervare.status = 'anulata'
+    rezervare.save()
+    
+    # 3. Ne intoarcem la lista
+    return redirect('lista_rezervari')
