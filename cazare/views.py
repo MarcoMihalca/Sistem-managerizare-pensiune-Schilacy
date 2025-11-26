67918207988ebc5f3fca1167af44081607b56fc8
Deyutzu <popenciucandrei@gmail.com>
2025-11-26 21:03:45 +0200

    am adaugat camere si am testat functionalitatea lor in homepage

--- cazare/views.py
+++ cazare/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import TipCamera  # <-- 1. Importam modelul (tabela)
 
-# Aceasta este functia pentru pagina principala
 def homepage(request):
-    return render(request, 'cazare/homepage.html')
+    # 2. Interogam baza de date: "Select * from TipCamera"
+    toate_tipurile = TipCamera.objects.all()
+    
+    # 3. Impachetam datele intr-un dictionar numit "context"
+    context = {
+        'tipuri': toate_tipurile
+    }
+    
+    # 4. Trimitem datele catre HTML
+    return render(request, 'cazare/homepage.html', context)
